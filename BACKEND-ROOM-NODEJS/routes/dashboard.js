4bfbf68c211bf6687b01cd02286e30a6d601b8ec
nxdminxx <nadzifahsamsudin@gmail.com>
2023-07-17 22:46:06 +0800

    vid 7 file up

---
+++ BACKEND-ROOM-NODEJS/routes/dashboard.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const connection = require('../connection');
+const router = express.Router();
+var auth = require('../services/authentication');
+
+router.get('/details',auth.authenticateToken,(req,res,next)=>{
+    var categoryCount;
+    var roomCount;
+    var billCount;
+
+    var query = "select count(id) as categoryCount from category";
+
+    connection.query(query,(err,result)=>{
+        
+        if(!err){
+            categoryCount = result[0].categoryCount;
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    })
+
+    var query = "select count(id) as roomCount from roomCount";
+
+    connection.query(query,(err,result)=>{
+        
+        if(!err){
+            roomCount = result[0].roomCount;
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    })
+
+    var query = "select count(id) as billCount from bill";
+
+    connection.query(query,(err,result)=>{
+        
+        if(!err){
+            billCount = result[0].billCount;
+
+            var data = {
+                category:categoryCount,
+                product:productCount,
+                bill:billCount
+            };
+            return res.status(200).json(data);
+
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    })
+
+})
+
+module.exports = router;
