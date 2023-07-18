2ab6936df5df5b13a9b1e4a23a556e3f74ce079f
nxdminxx <nadzifahsamsudin@gmail.com>
2023-07-18 21:04:42 +0800

    Stable code before installing ngx-ui-loader

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,60 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material-module';
+import { HomeComponent } from './home/home.component';
+import { BestSellerComponent } from './best-seller/best-seller.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SharedModule } from './shared/shared.module';
+import { FullComponent } from './layouts/full/full.component';
+import { AppHeaderComponent } from './layouts/full/header/header.component';
+import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SignupComponent } from './signup/signup.component';
+//import { NgxUiLoaderConfig, NgxUiLoaderModule, PB_DIRECTION, SPINNER } from 'ngx-ui-loader';
+
+/*
+const ngxUiLoaderConfig: NgxUiLoaderConfig ={ //spinner loader attribute
+  text:"loading....",
+  textColor:"#FFFFFF",
+  textPosition: "center-center",
+  pbColor:"red", //
+  bgsColor:"red", //
+  fgsColor:"red", //
+  fgsType: SPINNER.ballSpinClockwise, //jenis loader (refer website)
+  fgsSize:100,
+  pbDirection: PB_DIRECTION.leftToRight,
+  pbThickness:5
+}
+*/
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+    HomeComponent,
+    BestSellerComponent,
+    FullComponent,
+    AppHeaderComponent,
+    AppSidebarComponent,
+    SignupComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    SharedModule,
+    HttpClientModule,  /*for http req or res*/
+    //NgxUiLoaderModule.forRoot(ngxUiLoaderConfig)   // for spinner loader
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
