9a28ac66ccae02850eee7c9266ee3196cb12194f
Nurulaqilahmf <nurulaqilah.mf@graduate.utm.my>
2023-07-29 20:35:14 +0800

    product change to room

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -7,33 +7,40 @@ import { environment } from 'src/environments/environment';
 })
 export class ProductService {
   url = environment.apiUrl;
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  add(data:any){
-    return this.httpClient.post(this.url+"/room/add/",data,{
-      headers: new HttpHeaders().set('Content-Type',"application/json")
+  add(data: any) {
+    return this.httpClient.post(this.url + "/product/add/", data, {
+      headers: new HttpHeaders().set('Content-Type', "application/json")
     })
   }
 
-  update(data:any){
-    return this.httpClient.patch(this.url+"/room/update/",data,{
-      headers: new HttpHeaders().set('Content-Type',"application/json")
+  update(data: any) {
+    return this.httpClient.patch(this.url + "/product/update/", data, {
+      headers: new HttpHeaders().set('Content-Type', "application/json")
     })
   }
 
-  getProducts(){
-    return this.httpClient.get(this.url+"/room/get/")
+  getProducts() {
+    return this.httpClient.get(this.url + "/product/get/")
   }
 
-  updateStatus(data:any){
-    return this.httpClient.patch(this.url+"/room/updateStatus/",data,{
-      headers: new HttpHeaders().set('Content-Type',"application/json")
+  updateStatus(data: any) {
+    return this.httpClient.patch(this.url + "/product/updateStatus/", data, {
+      headers: new HttpHeaders().set('Content-Type', "application/json")
+    })
+  }
+  delete(id: any) {
+    return this.httpClient.delete(this.url + "/product/delete/" + id, {
+      headers: new HttpHeaders().set('Content-Type', "application/json")
     })
   }
 
-  delete(id:any){
-    return this.httpClient.patch(this.url+"/room/delete/"+id,{
-      headers: new HttpHeaders().set('Content-Type',"application/json")
-    })
+  getProductsByCategory(id: any) {
+    return this.httpClient.get(this.url + "/product/getByCategory/" + id);
+  }
+
+  getById(id: any) {
+    return this.httpClient.get(this.url + "/product/getById/" + id);
   }
 }
